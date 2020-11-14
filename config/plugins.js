b0ae262b8d7db378ed7115658afa140eda280676
Fergus Farrell <fergus.farrell@comrad.co.nz>
2020-11-15 12:26:31 +1300

    updated plugin to resolve encoded env vars

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,17 @@
+
+const getServiceAccount = (base64EncodedServiceAccount) => {
+  const buff = Buffer.from(base64EncodedServiceAccount, 'base64');
+  const serviceAccount = JSON.parse(buff.toString('utf-8'));
+  return serviceAccount;
+};
+
 module.exports = {
   upload: {
     provider: 'google-cloud-storage',
     providerOptions: {
       bucketName: process.env.GCP_BUCKET_NAME,
       baseUrl: process.env.GCP_BASE_URL,
-      serviceAccount: process.env.GCP_SERVICE_ACCOUNT,
+      serviceAccount: getServiceAccount(process.env.GCP_SERVICE_ACCOUNT)
     },
   },
 }
