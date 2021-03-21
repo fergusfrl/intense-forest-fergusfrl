717c70256114a7cd12afe92a94c0e2ccfc5a45fd
Fergus Farrell <fergus.farrell@comrad.co.nz>
2021-03-21 17:28:37 +1300

    added email support for new blogs

--- api/blog/models/blog.js
+++ api/blog/models/blog.js
@@ -1,10 +1,40 @@
 'use strict';
 const axios = require('axios');
+const firebase = require('firebase');
+const sgMail = require('@sendgrid/mail');
+
+// TODO: initialize firebase application
+firebase.initializeApp({
+  apiKey: process.env.GCP_API_KEY,
+  projectId: process.env.GCP_PROJECT_ID,
+  // databaseURL: process.env.GCP_DATABASE_URL,
+});
+
+sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+const database = firebase.firestore();
 
 const triggerBuildHook = async item => {
   if (item.published_at !== null) {
-    await axios.post(
-      strapi.config.get('staticWebsiteBuildURL') || process.env.NETLIFY_BUILD_HOOK, {});
+    await axios.post(strapi.config.get('staticWebsiteBuildURL') || process.env.NETLIFY_BUILD_HOOK, {});
+  }
+};
+
+const handleCreateUpdate = async item => {
+  triggerBuildHook(item);
+  if (item.finalized) {
+    const subscribers = database.collection('subscribers').get();
+    subscribers.forEach(sub => {
+      const email = sub.get('email');
+      sgMail.send({
+        to: email,
+        from: process.env.SEND_GRID_FROM_EMAIL,
+        templateId: process.env.SEND_GRID_TEMPLATE_ID,
+        dynamicTemplateData: {
+          blogTitle: item.title,
+          blogSubtitle: item.blurb,
+        },
+      });
+    });
   }
 };
 
@@ -14,8 +44,8 @@ const triggerBuildHook = async item => {
  */
 module.exports = {
   lifecycles: {
-    afterCreate: triggerBuildHook,
-    afterUpdate: triggerBuildHook,
+    afterCreate: handleCreateUpdate,
+    afterUpdate: handleCreateUpdate,
     afterDelete: triggerBuildHook,
   }
 };
