509115f90b46d19f75e84572b9dbbfe0994feb16
Fergus Farrell <fergus.farrell@comrad.co.nz>
2021-03-21 20:14:47 +1300

    moved initialization into bootstrap

--- api/blog/models/blog.js
+++ api/blog/models/blog.js
@@ -1,17 +1,5 @@
 'use strict';
 const axios = require('axios');
-// const firebase = require('firebase');
-const admin = require('firebase-admin');
-const sgMail = require('@sendgrid/mail');
-
-admin.initializeApp({
-  apiKey: process.env.GCP_API_KEY,
-  projectId: process.env.GCP_PROJECT_ID,
-  // databaseURL: process.env.GCP_DATABASE_URL,
-});
-
-sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
-const database = admin.firestore();
 
 const triggerBuildHook = async item => {
   if (item.published_at !== null) {
@@ -22,10 +10,14 @@ const triggerBuildHook = async item => {
 const handleCreateUpdate = async item => {
   triggerBuildHook(item);
   if (item.finalized) {
-    const subscribers = database.collection('subscribers').get();
+    const subscribers = await strapi.firebase
+      .firestore()
+      .collection('subscribers')
+      .get();
+
     subscribers.forEach(sub => {
       const email = sub.get('email');
-      sgMail.send({
+      strapi.sendGrid.send({
         to: email,
         from: process.env.SEND_GRID_FROM_EMAIL,
         templateId: process.env.SEND_GRID_TEMPLATE_ID,
--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,6 @@
 'use strict';
+const admin = require('firebase-admin');
+const sgMail = require('@sendgrid/mail');
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +12,14 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+  sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+  admin.initializeApp({
+    apiKey: process.env.GCP_API_KEY,
+    projectId: process.env.GCP_PROJECT_ID,
+    // databaseURL: process.env.GCP_DATABASE_URL,
+  });
+
+  strapi.firebase = admin;
+  strapi.sendGrid = sgMail;
+};
