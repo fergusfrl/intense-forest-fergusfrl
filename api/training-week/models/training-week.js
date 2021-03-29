4b34ee949e79d288e5839aa65c621b635455f9fe
Fergus Farrell <fergus.farrell@comrad.co.nz>
2021-03-29 15:53:45 +1300

    added training-week build hooks

--- api/training-week/models/training-week.js
+++ api/training-week/models/training-week.js
@@ -1,9 +1,19 @@
 'use strict';
+const axios = require('axios');
+
+const triggerBuildHook = async item => {
+  if (item.published_at !== null) {
+    await axios.post(strapi.config.get('staticWebsiteBuildURL') || process.env.NETLIFY_BUILD_HOOK, {});
+  }
+};
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
-
- // TODO: await axios.post(strapi.config.get('staticWebsiteBuildURL') || process.env.NETLIFY_BUILD_HOOK, {});
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    afterCreate: triggerBuildHook,
+    afterUpdate: triggerBuildHook,
+  },
+};
